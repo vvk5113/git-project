493618419152ebbb01ae657c943e1628fa4275f8
“vkumar” <kumar.vijay@pennmutual.com>
2015-11-06 11:15:35 -0500

    committing my second change

--- gittest/src/main/java/com/example/controller/MyTest.java
+++ gittest/src/main/java/com/example/controller/MyTest.java
@@ -1,5 +1,6 @@
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("Hi Vijay, how are your doing !!!");
+		System.out.println("Hi Mark, you are a cool man !!!");
 	}
 }
